2b4c3d50251b1bff3ac706c8dcca6e2938851de1
aaidenplays <aaidenplays@yahoo.com>
2020-04-22 16:31:00 -0500

    started controllers

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -1,2 +1,25 @@
 class AssignmentsController < ApplicationController
+    def index
+        assignments = Assignment.all
+        render json: assignments
+    end
+
+    def create
+
+    end
+
+    def show
+        assignment = Assignment.find_by(id: params[:id])
+    end
+
+    def update
+
+    end
+
+    def destroy
+    
+    end
+
+    private
+
 end
